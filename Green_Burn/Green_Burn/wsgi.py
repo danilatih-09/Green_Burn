8efcb8c4ab03e2efe943f3aec58879d7caecbea8
Danik <danilatihonovic@gmail.com>
2026-06-22 22:43:51 +0300

    collectstatic in wsgi

--- Green_Burn/Green_Burn/wsgi.py
+++ Green_Burn/Green_Burn/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Green_Burn.settings')
+
+# запускаем collectstatic автоматически при старте — нужно для Railway,
+# так как статические файлы не сохраняются между деплоями
+from django.core.management import call_command
+try:
+    call_command('collectstatic', '--noinput', verbosity=0)
+except Exception:
+    pass
 
 application = get_wsgi_application()
